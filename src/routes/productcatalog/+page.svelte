ce1ca5b36b6f21316a61a02d62fd5de98032ac31
Thomas Vautrin <thomas.vautrin@orange.com>
2023-01-20 11:47:03 +0100

    Clean

--- src/routes/productcatalog/+page.svelte
+++ src/routes/productcatalog/+page.svelte
@@ -2,43 +2,6 @@
     import Product from './Product.svelte'
 
     export let data;
-    let test = [{
-        id: 30,
-        title: 'Key Holder',
-        description: 'Attractive DesignMetallic materialFour key hooksReliable & DurablePremium Quality',
-        price: 30,
-        discountPercentage: 2.92,
-        rating: 4.92,
-        stock: 54,
-        brand: 'Golden',
-        category: 'home-decoration',
-        thumbnail: 'https://i.dummyjson.com/data/products/30/thumbnail.jpg',
-        images: [
-            'https://i.dummyjson.com/data/products/30/1.jpg',
-            'https://i.dummyjson.com/data/products/30/2.jpg',
-            'https://i.dummyjson.com/data/products/30/3.jpg',
-            'https://i.dummyjson.com/data/products/30/thumbnail.jpg'
-        ]
-    }, {
-        id: 30,
-        title: 'Key Holder',
-        description: 'Attractive DesignMetallic materialFour key hooksReliable & DurablePremium Quality',
-        price: 30,
-        discountPercentage: 2.92,
-        rating: 4.92,
-        stock: 54,
-        brand: 'Golden',
-        category: 'home-decoration',
-        thumbnail: 'https://i.dummyjson.com/data/products/30/thumbnail.jpg',
-        images: [
-            'https://i.dummyjson.com/data/products/30/1.jpg',
-            'https://i.dummyjson.com/data/products/30/2.jpg',
-            'https://i.dummyjson.com/data/products/30/3.jpg',
-            'https://i.dummyjson.com/data/products/30/thumbnail.jpg'
-        ]
-    }
-    ]
-
 </script>
 <div class="album py-5 bg-light">
     <div class="container">
--- src/routes/productcatalog/+page.ts
+++ src/routes/productcatalog/+page.ts
@@ -1,6 +1,5 @@
 export async function load({ fetch }) {
     const res = await fetch(`https://dummyjson.com/products/`);
     const items = await res.json();
-    const products =items.products;
-    return { products: products };
+    return { products: items.products };
 }
